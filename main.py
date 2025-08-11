90eb6b517fe32645a0307887d10da0921bf842cb
SALA COMPUTO SEDE CENTRO - BARRANQUILLA <salacomputocentro.baq@unilibre.edu.co>
2025-08-11 14:22:07 -0500

    Añadir set_done y remove_task en TaskModel

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from task_model import task_model
+from task_model import TaskModel
 
 def main():
-    task = TaskModel ("Estudiar para el examen")
-    print (f"Tarea Creada: {task.get_task_name()}")
+    task = TaskModel("Estudiar para el examen")
+    print(f"Tarea creada: {task.get_task_name()}")
+    task.set_done()
+    print(f"Tarea completada: {task.is_done()}")
+    task.remove_task()
+    print(f"Tarea eliminada: {task.get_task_name()}")
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
